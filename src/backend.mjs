75eaefc6bd10d3972a959d606b89ef9e179863b5
jehat12 <jehat.bakiray@edu.univ-fcomte.fr>
2026-01-30 11:55:25 +0100

    tp3

--- src/backend.mjs
+++ src/backend.mjs
@@ -1,6 +1,5 @@
 import PocketBase from "pocketbase";
 
-// ⚠️ mets l'URL de TON PocketBase (local le plus classique)
 const pb = new PocketBase("http://127.0.0.1:8090");
 
 export async function getOffres() {
