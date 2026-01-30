ac966eb9cb7e6f56e5ce7280c05f52c88bcdf412
jehat12 <jehat.bakiray@edu.univ-fcomte.fr>
2026-01-30 11:36:37 +0100

    feat(tp): PocketBase images + OffreCard simplified + favorites filter client-side

---
+++ src/backend.mjs
@@ -0,0 +1,20 @@
+import PocketBase from "pocketbase";
+
+const pb = new PocketBase("http://127.0.0.1:8090"); // <- change si besoin
+
+export async function getOffres() {
+  try {
+    const data = await pb.collection("maison").getFullList({
+      sort: "-created",
+    });
+    return data;
+  } catch (error) {
+    console.log("Une erreur est survenue en lisant la liste des maisons", error);
+    return [];
+  }
+}
+
+export async function getImageUrl(record, recordImage) {
+  if (!record || !recordImage) return "";
+  return pb.files.getURL(record, recordImage);
+}
