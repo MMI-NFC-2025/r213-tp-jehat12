c060ae80a173c2a47f39b7ba2f62b098ff8c4135
jehat12 <jehat.bakiray@edu.univ-fcomte.fr>
2026-01-30 11:46:32 +0100

    changements

--- src/backend.mjs
+++ src/backend.mjs
@@ -1,19 +1,20 @@
 import PocketBase from "pocketbase";
 
-const db = new PocketBase("http://127.0.0.1:8090"); // <- change si besoin
+// ⚠️ mets l'URL de TON PocketBase (local le plus classique)
+const pb = new PocketBase("http://127.0.0.1:8090");
 
 export async function getOffres() {
   try {
-    const data = await db.collection("maison").getFullList({
+    const data = await pb.collection("maison").getFullList({
       sort: "-created",
     });
     return data;
   } catch (error) {
-    console.log("Une erreur est survenue en lisant la liste des maisons", error);
+    console.log("Erreur getOffres()", error);
     return [];
   }
 }
 
 export function getImageUrl(record, recordImage) {
-  return db.files.getURL(record, recordImage);
+  return pb.files.getURL(record, recordImage);
 }
